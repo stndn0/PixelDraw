ec0e8272e86583e48244034628aa28879c8e9c9a
st <st@st.com>
2022-07-08 13:51:28 +1000

    added proper grid scaling

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
 const PIXEL_COUNT = 1024
-const pixelWidth = 16
-const pixelHeight = 16
 let pixels = []
 
 // DOM elements
 const canvas = document.getElementById("canvas")
 const gridSizeSlider = document.getElementById("myRange");
 const gridSizeText = document.getElementById("grid-size");
+
+let canvasWidth = getComputedStyle(document.querySelector('#canvas')).width;
+let canvasHeight = getComputedStyle(document.querySelector('#canvas')).height;
+canvasWidth = parseInt(canvasWidth)         // Remove trailing 'px' from string
+canvasHeight = parseInt(canvasHeight)
 
 
 // Load canvas information. Populate grid with unique pixels.
@@ -31,24 +34,24 @@ gridSizeSlider.oninput = function () {
 window.addEventListener("mouseover", function (e) {
     // Where was the click?
     let target = e.target;
-    console.log("DEBUG: Current target:", target.id)
+    // console.log("DEBUG: Current target:", target.id)
 
     // If user only clicks on one pixel
     canvas.addEventListener('mousedown', (event) => {
-        console.log("DEBUG: Mouse is DOWN")
+        // console.log("DEBUG: Mouse is DOWN")
         let target = event.target;
         if (target.className == "pixel") {
             document.getElementById(target.id).style.backgroundColor = "red";
         }
     })
 
-    canvas.addEventListener('mouseup', (event) => {
-        console.log("DEBUG: Mouse is UP")
-    })
+    // canvas.addEventListener('mouseup', (event) => {
+    //     console.log("DEBUG: Mouse is UP")
+    // })
 
     // If user holds down LMB on multiple pixels
     if (e.buttons == 1 || e.buttons == 3) {
-        console.log("DEBUG: Mouse LEFT CLICK")
+        // console.log("DEBUG: Mouse LEFT CLICK")
         if (target.className == "pixel") {
             document.getElementById(target.id).style.backgroundColor = "red";
         }
@@ -56,7 +59,7 @@ window.addEventListener("mouseover", function (e) {
 })
 
 
-function setCanvasParams(pixelWidth, pixelHeight, PIXEL_COUNT) {
+function setCanvasParams(gridWidth, gridHeight, PIXEL_COUNT) {
     // Delete existing divs
     const pixelDivs = document.querySelectorAll('.pixel');
     pixelDivs.forEach(pixel => {
@@ -64,12 +67,23 @@ function setCanvasParams(pixelWidth, pixelHeight, PIXEL_COUNT) {
     })
     pixels = []
 
+    // Calculate how many pixels this 
+
+
     for (let i = 0; i < PIXEL_COUNT; i++) {
+        // Create div (pixel block) and give it a class and id
         pixels[i] = document.createElement('div')
         pixels[i].className = 'pixel'
         pixels[i].id = 'pixel' + i
+
+        // GridWidth is the value from the grid size slider control.
+        // Our canvas width is 800px. We want gridWidth number of pixels to fill the width.
+        // Find the correct pixel size for gridWidth number of pixels to fill the width.
+        // Note - subtract 2px. This is the left and right border for a pixel block.
+        pixels[i].style.width = (canvasWidth / gridWidth) - 2 + "px";
+        pixels[i].style.height = (canvasHeight / gridHeight) - 2 + "px";
+
+        // Add div to the document
         document.getElementById("canvas").appendChild(pixels[i])
-        pixels[i].style.width = pixelWidth / 2  + "px"
-        pixels[i].style.height = pixelHeight / 2 + "px"
     }
 }
